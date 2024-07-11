83673cda796b78044178c5467e3af045e0b85d98
Kdwan004 <kush.dewan004@gmail.com>
2024-07-11 16:10:17 +1000

    Slight change

--- Tests/test_subject.py
+++ Tests/test_subject.py
@@ -3,28 +3,11 @@ import unittest
 from unittest.mock import patch
 
 sys.path.insert(1, '/home/bleachly/Desktop/tasks')
-
 import subject
 
-class TestSubject(unittest.TestCase):
-
-    # Initialise mock input
-    def setUp(self):
-        self.subject_list = []
-    
-
-    # @Path replaces real object in code with mock instances
-    # Test the input of 1 subject
-    @patch('builtins.input', side_effect=["Math", "yes"])
-    def test_add_subject(self, mock_input):
-        subject.list_subject(self.subject_list)
-        self.assertEqual(self.subject_list, ["Math"])
-    
-    # Test if user decided to rename subject
-    @patch('builtins.input', side_effect=["", "no", "English", "yes"])
-    def test_redo_subject(self, mock_input):
-        subject.list_subject(self.subject_list)
-        self.assertEqual(self.subject_list, ["English"])
+'''
+I shall get back to these test cases heehee hoohoo
+'''
 
 
     
